35b74a54ad84b25a1e0d44040c85fb462326bb35
Farbod Rezabakhsh <farbod.rezabakhsh80@gmail.com>
2025-01-25 00:30:50 +0330

    setup postgresql database

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -11,11 +11,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read .env file
+env_file_path = BASE_DIR / '.env'
+if env_file_path.exists():
+    with open(env_file_path) as f:
+        for line in f:
+            # Ignore comments and empty lines
+            if line.startswith('#') or not line.strip():
+                continue
+            # Split each line into key-value pairs
+            key, value = line.strip().split('=', 1)
+            os.environ[key] = value
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -81,8 +92,12 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
